6cab81acb1dd7be831ec2cf0aeb8c0dba9873f22
Mari1797 <marianisidahomen@gmail.com>
2022-06-04 13:48:57 -0700

    pls nah

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/**
+ * main - Prints a file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of a art,\n"
+);
+	return (0)
+}
