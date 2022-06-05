117990e5774287780b017fe4ddc8fa1e7160b1ce
root <marianisidahomen@gmail.com>
2022-06-05 13:36:39 -0700

    fighting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 
+
 #include <stdio.h>
 
+
 /**
- * main - Prints a file
+ * main - Prints a text
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of a art,\n"
+	printf("with proper grammar,but the outcome is a piece of art,\n"
 );
 	return (0)
 }
