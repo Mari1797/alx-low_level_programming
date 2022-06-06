2a2ba145beebbcbf5f08e8408d767cdd7081e4e3
root <marianisidahomen@gmail.com>
2022-06-06 04:21:54 -0700

    grit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+
+/**
+ * main - Prints the numbers from 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       char n;
+
+       for (n = '0'; n <= '9'; n++)
+       {
+	       putchar(n);
+       }
+
+       putchar('\n');
+
+       return (0);
+}
+
