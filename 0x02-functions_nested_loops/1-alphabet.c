3a1f71b676656fe77c664ad58af8305c68d40e1c
root <marianisidahomen@gmail.com>
2022-06-08 05:26:42 -0700

    come on

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints lowercase letters
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'i'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
