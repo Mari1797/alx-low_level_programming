d75875467aee4b49769029648a74f685bb2c622d
root <marianisidahomen@gmail.com>
2022-06-08 20:01:22 -0700

    fight

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,11 @@
 
 
 /**
- * _islower - Check if c is a lower character
- * @c: The character to be checked 
- *
- * Return: 1 if c is a lower character or 0 if c isd not a lowercharacter
+ * _islower - Checks for lower character
+ * Return: 1 if c is a lower character or 0 if c is ptherwise
+ * @c: character to be checked
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-
-	return (0);
+	return (c >= 'a' && c <= 'z');
 }
