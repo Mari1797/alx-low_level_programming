efe05430622ceccfda221a19f6cb6e66a7d08d26
root <marianisidahomen@gmail.com>
2022-06-10 01:41:41 -0700

    task

---
+++ 0x03-debugging/-main.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+
+
+/**
+ *
+ * main - tests function if integer is positive or negative
+ * Return: 0
+ */
+
+int main (void)
+{
+	int i;
+
+	I = 0
+	positive_or_negative(I);
+
+	return (0);
+}
+
