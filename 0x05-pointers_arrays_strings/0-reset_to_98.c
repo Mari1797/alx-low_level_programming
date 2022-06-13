6613d4a195f9c4773db0595dc770d5116d91998b
Mari1797 <marianisidahomen@gmail.com>
2022-06-13 11:32:49 -0700

    reset to m98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes int as a parameter and prints the value up to 98
+ * @n: parameter to be taken
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
