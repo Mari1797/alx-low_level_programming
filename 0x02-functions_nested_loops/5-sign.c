d5b2ca8d605c50e43898d3e4f41ebf259c845d95
Mari1797 <marianisidahomen@gmail>
2022-06-13 02:57:31 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		-putchar('+');
+		-putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(49);
 		return (-1);
 	}
 	else
 	{
-		_putchar('0');
+		_putchar(54);
 		return (0);
 	}
 }
