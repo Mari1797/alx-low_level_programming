047872adeed04f0ce3c272d39740ad208dbc188f
Mari1797 <marianisidahomen@gmail.com>
2022-06-13 13:53:31 -0700

    indent

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i -=; i >=0; c--)
+	{
+		_putchar(s[i]);	
+	}
+
+        _putchar('\n');
+}
+
