53b3a9cf8d5fdd80df819843f52e5ef46bf7d1a2
Mari1797 <marianisidahomen@gmail.com>
2022-06-17 07:26:35 -0700

    come on

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-
-#include <stdio.h>
-
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -18,4 +11,3 @@ char *rot13(char *);
 void print_number(int n);
 
 
-#endif /* MAIN_H */
