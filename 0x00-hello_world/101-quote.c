2ae33112309dc137d40814ea9d1ccca155ae86f7
Mari1797 <marianisidahomen@gmail.com>
2022-09-08 21:32:29 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd>
 
 /**
  * main - Prints a quote by Dora Korpar
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19 \n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 
 	return (1);
 
