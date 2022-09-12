576692a1e815cc492984d506df01ff67e170dfd0
Mari1797 <marianisidahomen@gmail.com>
2022-09-12 13:31:51 -0700

    corrected n to ld

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
-	if (n > 5)
+	if (ld > 5)
 	{
 		printf("ld of %d is %d and id greater than 5\n", n, ld);
 	}
-	else if (n == 0)
+	else if (ld == 0)
 	{
 		printf("ld of %d is %d and id is 0\n", n, ld);
 	}
-	else if (n < 6 && n != 0)
+	else if (ld < 6 && n != 0)
 	{
 		printf("ld of %d is %d and id is less than 6 and not 0\n", n, ld);
 	}
