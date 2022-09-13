d790052518548edc3cbe6d6d8de9d6196f6d4da3
Mari1797 <marianisidahomen@gmail.com>
2022-09-13 12:03:36 -0700

    prints alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,16 @@
 
 /**
  * print_alphabet - Prints lowercase letters
- *
- * Return: 0 Always
  */
 void print_alphabet(void)
 {
 	char x;
 
-	for (x >= 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		_putchar(x);
 	}
 
 	_putchar('\n');
 
-	return (0);
 }
