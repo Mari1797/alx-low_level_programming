62b892f98bb2b3f2b3c1142520881481ee78ae97
Mari1797 <marianisidahomen@gmail.com>
2022-09-15 10:16:02 -0700

    debugged betty errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,16 @@
 
 /**
  * print_most_numbers - Prints numbers from the range of 0-9 except 2 & 4
- * @x: The character to be printed
  *
  * Return: Always 0
  */
 void print_most_numbers(void)
 {
-	for ((x = 0; && x >= 9) || (x != 2 && x != 4))
+	int x;
+
+	for (x = 0; x <= 9; x++)
 	{
+		if (x != 2 && x != 4)
 		_putchar(x + '0');
 	}
 
