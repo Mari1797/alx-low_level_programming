c3a06651363982080fb121eedb6366cf223305c3
Mari1797 <marianisidahomen@gmail.com>
2022-09-15 08:57:18 -0700

    changed x to c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 
 
 /**
- * _isdigit - checks for a digit (o through 9)
- * @x: the character to be checked
+ * _isdigit - checks for a digit (0 - 9)
+ * @c: the character to be checked
  *
  * Return: 1 if character is a digit or 0 if otherwise
  */
-int _isdigit(int x)
+int _isdigit(int c)
 {
-	if (x >= 0 && x <= 9)
+	if (c >= 0 && c <= 9)
 	{
 		return (1);
 	}
