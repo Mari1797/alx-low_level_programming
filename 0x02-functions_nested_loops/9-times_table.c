4bffa303a109e7cbd5d9702bb296fcb6c469e292
Mari1797 <marianisidahomen@gmail.com>
2022-09-22 12:59:10 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table
+ *
+ * Return: Always (Success)
+ */
+void times_table(void)
+{
+	int n;
+	int m;
+	int pd;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(0);
+
+		for (m = 0; m <= 9; m++)
+		{
+			putchar(',');
+			putchar(' ');
+
+			pd = n * m;
+
+			if (pd <= 9)
+				putchar(' ');
+			else
+				putchar((pd / 10) + 0);
+
+			putchar((pd % 10) + 0);
+		}
+		putchar('\n');
+	}
+}
