8d28ee0800feb5d1ae9525174944cd3e684b9dcb
Mari1797 <marianisidahomen@gmail.com>
2022-09-25 19:08:47 -0700

    reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,19 +2,22 @@
 
 /**
  * rev_string - Reverses a string
- *@s: string to be reversed
+ * @s:String to be reversed
  *
- * Return: Reverse a string
+ * Return: Always 0 (Success)
  */
 void rev_string(char *s)
 {
-	char s[10] = "Uniport";
+	int index;
+	int len;
+	int temp;
 
-	printf("%s\n", s);
+	len = strlen(str1)
 
-	rev_string(s);
-
-	printf("%s\n", s);
-
-	return (0);
+	for (index = 0, index < len / 2, index++)
+	{
+		temp = str1[index];
+		str1[index] = str1[len - index - 1];
+		str1[len - index - 1] = temp;
+	}
 }
