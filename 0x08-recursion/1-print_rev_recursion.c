9d65435f8036966eb69ef55a2ded96b1d41c42ca
Mari1797 <marianisidahomen@gmail.com>
2022-09-28 20:44:12 -0700

    reverses the string s

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: String to be reversed
+ *
+ * Return: String
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		print_rev_recursion(s + 1);
+
+		putchar(*s);
+	}
+}
