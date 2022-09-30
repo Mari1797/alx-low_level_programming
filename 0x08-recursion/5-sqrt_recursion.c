065bcb9393b057cc1e811c6c92d39f5ffb51d5b9
Mari1797 <marianisidahomen@gmail.com>
2022-09-29 18:07:14 -0700

    calculating the square of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int guess);
+
+/**
+ * _sqrt_recursion - Returns the natural root of a number
+ * @n: The number whose root is to be calculated
+ *
+ * Return: Thew natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, guess));
+}
+
+/**
+ * _sqrt - Calculates the natural square root of a number
+ * @n: Number whose square root is to be calculated
+ * @guess: Guess value
+ *
+ * Return: The natural square root ofd the number
+ */
+int _sqrt(int n, int guess)
+{
+	if (n / guess != guess)
+		return (-1);
+	else if (n / guess == guess)
+		return (guess);
+	else
+		return (_sqrt(guess + n / guess) / 2);
+}
