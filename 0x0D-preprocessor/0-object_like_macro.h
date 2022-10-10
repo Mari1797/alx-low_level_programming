0eff8176bea09afbdf33bbb0e1f4f3db58331254
Mari1797 <marianisidahomen@gmail.com>
2022-10-10 13:49:25 -0700

    first header file

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Description: creates a header file that defines a macro named SIZE
+ * File: 0-object_like_macro.h
+ * Author: MI
+ * @SIZE: Abbreviation to token
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Learning about cmpilation and preprocessor
