b1016e643f5ecfedd2aa731fda2fe7431fa468af
Mari1797 <marianisidahomen@gmail.com>
2022-10-21 18:42:37 -0700

    made corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,16 +3,16 @@
 /**
  * _memset - fills a memory with a constant byte
  * @s: pointer to the memory
- * @c: characte to be filled
+ * @b: characte to be filled
  * @n: number of bytes
  * Return: pointer to be filled with memory
  */
-void *_memset(void *s, int c, size_t n)
+void *_memset(void *s, int b, unsigned int n)
 {
 	unsigned int index;
 	unsigned char;
 	*memory = s;
-	value = c;
+	value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
